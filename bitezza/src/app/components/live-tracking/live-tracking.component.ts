3f2f0e01066c7a229919c35e261c0bb4f5b36715
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-06 21:28:40 +0530

    Design order status columns (Preparing, Cooking, Ready)

--- bitezza/src/app/components/live-tracking/live-tracking.component.ts
+++ bitezza/src/app/components/live-tracking/live-tracking.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LiveTrackerHeaderComponent } from "../live-tracker-header/live-tracker-header.component";
 import { PageTitleComponent } from "../page-title/page-title.component";
+import { OrderColumnHeaderComponent } from "../order-column-header/order-column-header.component";
 
 @Component({
   selector: 'app-live-tracking',
-  imports: [LiveTrackerHeaderComponent, PageTitleComponent],
+  imports: [LiveTrackerHeaderComponent, PageTitleComponent, OrderColumnHeaderComponent],
   templateUrl: './live-tracking.component.html',
   styleUrl: './live-tracking.component.css'
 })
---
+++ bitezza/src/app/components/order-column-header/order-column-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-column-header',
+  imports: [],
+  templateUrl: './order-column-header.component.html',
+  styleUrl: './order-column-header.component.css'
+})
+export class OrderColumnHeaderComponent {
+
+}
