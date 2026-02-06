abd696684f94572547229dbad1fe6a690fc32334
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-06 16:09:48 +0530

    Add live tracking section page title

--- bitezza/src/app/components/live-tracking/live-tracking.component.ts
+++ bitezza/src/app/components/live-tracking/live-tracking.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LiveTrackerHeaderComponent } from "../live-tracker-header/live-tracker-header.component";
+import { PageTitleComponent } from "../page-title/page-title.component";
 
 @Component({
   selector: 'app-live-tracking',
-  imports: [LiveTrackerHeaderComponent],
+  imports: [LiveTrackerHeaderComponent, PageTitleComponent],
   templateUrl: './live-tracking.component.html',
   styleUrl: './live-tracking.component.css'
 })
