a32f1bd6b8eba5950627eae34e85fdf226c074ca
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-06 11:34:23 +0530

    Add interactive product card for menu items

--- bitezza/src/app/pages/home/home.component.ts
+++ bitezza/src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { SidebarComponent } from "../../components/sidebar/sidebar.component";
 import { UserProfileComponent } from "../../components/user-profile/user-profile.component";
 import { HeaderComponent } from "../../components/header/header.component";
 import { ProductFilterComponent } from "../../components/product-filter/product-filter.component";
+import { ProductCardComponent } from "../../components/product-card/product-card.component";
 
 @Component({
   selector: 'app-home',
-  imports: [SidebarComponent, UserProfileComponent, HeaderComponent, ProductFilterComponent],
+  imports: [SidebarComponent, UserProfileComponent, HeaderComponent, ProductFilterComponent, ProductCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
