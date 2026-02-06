b3185f1c7ff1d49156ebde66ffeffa31a78c752e
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-06 12:03:51 +0530

    Add interactive cart component with totals and actions

---
+++ bitezza/src/app/components/cart/cart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CartItemComponent } from "../cart-item/cart-item.component";
+
+@Component({
+  selector: 'app-cart',
+  imports: [CartItemComponent],
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.css'
+})
+export class CartComponent {
+
+}
