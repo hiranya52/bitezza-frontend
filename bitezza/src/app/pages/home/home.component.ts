d9d36f9d0985840206b3de7e5786c844ac0fa3bd
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-06 11:26:56 +0530

    Redesign sidebar with updated order and management links

--- bitezza/src/app/pages/home/home.component.ts
+++ bitezza/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "../../components/sidebar/sidebar.component";
 import { UserProfileComponent } from "../../components/user-profile/user-profile.component";
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-home',
-  imports: [SidebarComponent, UserProfileComponent],
+  imports: [SidebarComponent, UserProfileComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
