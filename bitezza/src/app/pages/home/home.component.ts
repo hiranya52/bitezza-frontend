1a0342e0bc71661235e34513c49bec8ae06170cb
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-06 11:31:42 +0530

    Add product filter chips for category selection

--- bitezza/src/app/pages/home/home.component.ts
+++ bitezza/src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from "../../components/sidebar/sidebar.component";
 import { UserProfileComponent } from "../../components/user-profile/user-profile.component";
 import { HeaderComponent } from "../../components/header/header.component";
+import { ProductFilterComponent } from "../../components/product-filter/product-filter.component";
 
 @Component({
   selector: 'app-home',
-  imports: [SidebarComponent, UserProfileComponent, HeaderComponent],
+  imports: [SidebarComponent, UserProfileComponent, HeaderComponent, ProductFilterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
