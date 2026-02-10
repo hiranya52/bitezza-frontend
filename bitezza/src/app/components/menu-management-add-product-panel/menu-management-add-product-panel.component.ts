b5d70f26b8b5d0851ec59b36e0e4af378f9abad9
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-10 21:14:09 +0530

    Design and add add-product side panel

---
+++ bitezza/src/app/components/menu-management-add-product-panel/menu-management-add-product-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-management-add-product-panel',
+  imports: [],
+  templateUrl: './menu-management-add-product-panel.component.html',
+  styleUrl: './menu-management-add-product-panel.component.css'
+})
+export class MenuManagementAddProductPanelComponent {
+
+}
