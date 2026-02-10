62640e38ab50663a990e9e4d95dc6c6c3c30d0d5
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-10 11:44:44 +0530

    Design and Add Order History page stats section

---
+++ bitezza/src/app/components/order-history-page-stats/order-history-page-stats.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-history-page-stats',
+  imports: [],
+  templateUrl: './order-history-page-stats.component.html',
+  styleUrl: './order-history-page-stats.component.css'
+})
+export class OrderHistoryPageStatsComponent {
+
+}
--- bitezza/src/app/components/order-history/order-history.component.ts
+++ bitezza/src/app/components/order-history/order-history.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { OrderColumnHeaderComponent } from "../order-column-header/order-column-header.component";
 import { OrderHistoryHeaderComponent } from "../order-history-header/order-history-header.component";
+import { OrderHistoryPageStatsComponent } from "../order-history-page-stats/order-history-page-stats.component";
 
 @Component({
   selector: 'app-order-history',
-  imports: [OrderColumnHeaderComponent, OrderHistoryHeaderComponent],
+  imports: [OrderColumnHeaderComponent, OrderHistoryHeaderComponent, OrderHistoryPageStatsComponent],
   templateUrl: './order-history.component.html',
   styleUrl: './order-history.component.css'
 })
