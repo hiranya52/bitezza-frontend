5b15020f54c5c5cf829779cd02e8534a4d23598b
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-10 14:52:37 +0530

    Implement sidebar link component with dynamic active state

---
+++ bitezza/src/app/components/menu-management-sidebar-item/menu-management-sidebar-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-management-sidebar-item',
+  imports: [],
+  templateUrl: './menu-management-sidebar-item.component.html',
+  styleUrl: './menu-management-sidebar-item.component.css'
+})
+export class MenuManagementSidebarItemComponent {
+
+}
