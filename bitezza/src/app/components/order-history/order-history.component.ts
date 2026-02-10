a514620a5bf9d596066f7e972c4be1fcb8a510a2
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-10 11:52:50 +0530

    Design and add order history filter bar

--- bitezza/src/app/components/order-history/order-history.component.ts
+++ bitezza/src/app/components/order-history/order-history.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { OrderColumnHeaderComponent } from "../order-column-header/order-column-header.component";
 import { OrderHistoryHeaderComponent } from "../order-history-header/order-history-header.component";
 import { OrderHistoryPageStatsComponent } from "../order-history-page-stats/order-history-page-stats.component";
+import { OrderHistoryFilterBarComponent } from "../order-history-filter-bar/order-history-filter-bar.component";
 
 @Component({
   selector: 'app-order-history',
-  imports: [OrderColumnHeaderComponent, OrderHistoryHeaderComponent, OrderHistoryPageStatsComponent],
+  imports: [OrderColumnHeaderComponent, OrderHistoryHeaderComponent, OrderHistoryPageStatsComponent, OrderHistoryFilterBarComponent],
   templateUrl: './order-history.component.html',
   styleUrl: './order-history.component.css'
 })
