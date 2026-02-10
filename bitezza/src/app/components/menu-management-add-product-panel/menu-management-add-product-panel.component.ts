8de21edea978045b72ba1d6b3078d1963fb1f447
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-10 21:36:30 +0530

    Add product form component in menu management add product panel

--- bitezza/src/app/components/menu-management-add-product-panel/menu-management-add-product-panel.component.ts
+++ bitezza/src/app/components/menu-management-add-product-panel/menu-management-add-product-panel.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MenuManagementImageUploadComponent } from "../menu-management-image-upload/menu-management-image-upload.component";
+import { MenuManagementProductFormComponent } from "../menu-management-product-form/menu-management-product-form.component";
 
 @Component({
   selector: 'app-menu-management-add-product-panel',
-  imports: [MenuManagementImageUploadComponent],
+  imports: [MenuManagementImageUploadComponent, MenuManagementProductFormComponent],
   templateUrl: './menu-management-add-product-panel.component.html',
   styleUrl: './menu-management-add-product-panel.component.css'
 })
