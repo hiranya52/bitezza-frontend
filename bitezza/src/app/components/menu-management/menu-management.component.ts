dabd4e7dcf46ed52e25d9a8fd498ab4abbcdc16f
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-10 21:41:03 +0530

    Add panel footer component in menu management main page

--- bitezza/src/app/components/menu-management/menu-management.component.ts
+++ bitezza/src/app/components/menu-management/menu-management.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MenuManagementImageUploadComponent } from "../menu-management-image-upload/menu-management-image-upload.component";
+import { MenuManagementProductFormComponent } from "../menu-management-product-form/menu-management-product-form.component";
+import { MenuManagementPanelFooterComponent } from "../menu-management-panel-footer/menu-management-panel-footer.component";
 
 interface SidebarLink {
   icon: string;
@@ -10,12 +13,17 @@ interface SidebarLink {
 @Component({
   selector: 'app-menu-management',
   standalone: true,
-  imports: [],
+  imports: [MenuManagementImageUploadComponent, MenuManagementProductFormComponent, MenuManagementPanelFooterComponent],
   templateUrl: './menu-management.component.html',
   styleUrl: './menu-management.component.css'
 })
 
 export class MenuManagementComponent {
+
+  close() {
+    // later you can emit an event here
+    console.log('close panel');
+  }
 
   links: SidebarLink[] = [
     { icon: 'dashboard', label: 'Dashboard', href: '#' },
