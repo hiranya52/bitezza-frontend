e565de2da9380a144ce1a98725c32d3823306edd
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-10 11:59:49 +0530

    Design and add order history orders table

---
+++ bitezza/src/app/components/order-history-orders-table/order-history-orders-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-history-orders-table',
+  imports: [],
+  templateUrl: './order-history-orders-table.component.html',
+  styleUrl: './order-history-orders-table.component.css'
+})
+export class OrderHistoryOrdersTableComponent {
+
+}
--- bitezza/src/app/components/order-history/order-history.component.ts
+++ bitezza/src/app/components/order-history/order-history.component.ts
@@ -3,10 +3,11 @@ import { OrderColumnHeaderComponent } from "../order-column-header/order-column-
 import { OrderHistoryHeaderComponent } from "../order-history-header/order-history-header.component";
 import { OrderHistoryPageStatsComponent } from "../order-history-page-stats/order-history-page-stats.component";
 import { OrderHistoryFilterBarComponent } from "../order-history-filter-bar/order-history-filter-bar.component";
+import { OrderHistoryOrdersTableComponent } from "../order-history-orders-table/order-history-orders-table.component";
 
 @Component({
   selector: 'app-order-history',
-  imports: [OrderColumnHeaderComponent, OrderHistoryHeaderComponent, OrderHistoryPageStatsComponent, OrderHistoryFilterBarComponent],
+  imports: [OrderColumnHeaderComponent, OrderHistoryHeaderComponent, OrderHistoryPageStatsComponent, OrderHistoryFilterBarComponent, OrderHistoryOrdersTableComponent],
   templateUrl: './order-history.component.html',
   styleUrl: './order-history.component.css'
 })
