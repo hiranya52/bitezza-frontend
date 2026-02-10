b5ee3b331c61d62c3797628662ef3a74fdb412db
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-10 14:30:39 +0530

    Design and add order history summary cards

--- bitezza/src/app/components/order-history/order-history.component.ts
+++ bitezza/src/app/components/order-history/order-history.component.ts
@@ -5,10 +5,11 @@ import { OrderHistoryPageStatsComponent } from "../order-history-page-stats/orde
 import { OrderHistoryFilterBarComponent } from "../order-history-filter-bar/order-history-filter-bar.component";
 import { OrderHistoryOrdersTableComponent } from "../order-history-orders-table/order-history-orders-table.component";
 import { OrderHistoryPaginationComponent } from "../order-history-pagination/order-history-pagination.component";
+import { OrderHistorySummaryCardsComponent } from "../order-history-summary-cards/order-history-summary-cards.component";
 
 @Component({
   selector: 'app-order-history',
-  imports: [OrderHistoryHeaderComponent, OrderHistoryPageStatsComponent, OrderHistoryFilterBarComponent, OrderHistoryOrdersTableComponent, OrderHistoryPaginationComponent],
+  imports: [OrderHistoryHeaderComponent, OrderHistoryPageStatsComponent, OrderHistoryFilterBarComponent, OrderHistoryOrdersTableComponent, OrderHistoryPaginationComponent, OrderHistorySummaryCardsComponent],
   templateUrl: './order-history.component.html',
   styleUrl: './order-history.component.css'
 })
