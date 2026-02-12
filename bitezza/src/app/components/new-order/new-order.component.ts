635080ccf58fbad8e13b5b23afa1b8a12fd0de04
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-12 14:37:16 +0530

    Configure router and HTTP client with zone change detection

--- bitezza/src/app/components/new-order/new-order.component.ts
+++ bitezza/src/app/components/new-order/new-order.component.ts
@@ -3,7 +3,6 @@ import { HeaderComponent } from "../header/header.component";
 import { ProductFilterComponent } from "../product-filter/product-filter.component";
 import { ProductCardComponent } from "../product-card/product-card.component";
 import { CartComponent } from "../cart/cart.component";
-import { SidebarComponent } from "../sidebar/sidebar.component";
 
 @Component({
   selector: 'app-new-order',
--- bitezza/src/app/service/products/product.service.ts
+++ bitezza/src/app/service/products/product.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { API_ENDPOINTS } from '../../../core/api/api-endpoints';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  getAllProducts(){
+    return this.http.get(API_ENDPOINTS.product.getAll);
+  }
+
 }
