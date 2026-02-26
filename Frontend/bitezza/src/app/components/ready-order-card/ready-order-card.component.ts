202f3235acf7966bcf51f27dd4a9af3a0855b98c
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-26 16:01:07 +0530

    Display productName list in ReadyOrderCard

--- Frontend/bitezza/src/app/components/ready-order-card/ready-order-card.component.ts
+++ Frontend/bitezza/src/app/components/ready-order-card/ready-order-card.component.ts
@@ -1,9 +1,10 @@
 import { order } from './../../../model/order.model';
 import { Component, Input } from '@angular/core';
+import { CommonModule, NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-ready-order-card',
-  imports: [],
+  imports: [CommonModule, NgIf, NgFor],
   templateUrl: './ready-order-card.component.html',
   styleUrl: './ready-order-card.component.css'
 })
