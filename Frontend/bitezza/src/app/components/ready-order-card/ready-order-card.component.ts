e06dbef702f7bfc10c0ec6021c28b53a1f9a53a7
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-26 22:24:20 +0530

    Refresh order list by calling getAllOrders after moving to FINISHED

--- Frontend/bitezza/src/app/components/ready-order-card/ready-order-card.component.ts
+++ Frontend/bitezza/src/app/components/ready-order-card/ready-order-card.component.ts
@@ -2,6 +2,7 @@ import { order } from './../../../model/order.model';
 import { Component, inject, Input } from '@angular/core';
 import { CommonModule, NgIf, NgFor } from '@angular/common';
 import { OrderService } from '../../service/orders/order.service';
+import { OrderColumnHeaderComponent } from '../order-column-header/order-column-header.component';
 
 @Component({
   selector: 'app-ready-order-card',
@@ -14,9 +15,11 @@ export class ReadyOrderCardComponent {
   @Input() order!: order;
 
   private orderService = inject(OrderService);
+  private orderColumn = inject(OrderColumnHeaderComponent);
 
   completeOrder(orderId: number){
     this.orderService.updateOrderStatus(orderId).subscribe( res=> {
+      this.orderColumn.getAllOrders();
     })
   }
 
