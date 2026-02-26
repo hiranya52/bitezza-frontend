f1f5e720c02b9049937b638f55c93f7395c2ffd6
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-26 16:02:36 +0530

    Display productName list in CookingOrderCard

--- Frontend/bitezza/src/app/components/cooking-order-card/cooking-order-card.component.ts
+++ Frontend/bitezza/src/app/components/cooking-order-card/cooking-order-card.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { order } from './../../../model/order.model';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cooking-order-card',
-  imports: [],
+  imports: [CommonModule, NgFor, NgIf],
   templateUrl: './cooking-order-card.component.html',
   styleUrl: './cooking-order-card.component.css'
 })
