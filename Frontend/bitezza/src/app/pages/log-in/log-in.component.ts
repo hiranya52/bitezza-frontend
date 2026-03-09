31d34cd8314f658a34b1e7520e914fa7baca8d54
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-03-09 16:15:19 +0530

    Implement reactive login form with FormBuilder and form submission

--- Frontend/bitezza/src/app/pages/log-in/log-in.component.ts
+++ Frontend/bitezza/src/app/pages/log-in/log-in.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-log-in',
-  imports: [],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css'
 })
 export class LogInComponent {
 
-  
+  userForm: FormGroup
+
+  constructor(
+    formBuider: FormBuilder
+  ){
+
+    this.userForm = formBuider.group({
+      username: [''],
+      password: ['']
+    })
+
+  }
+
+
+  onSubmit(){
+    
+  }
+
+
 
 }
