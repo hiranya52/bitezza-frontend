94a15d8b93ce97cf3b6a58b066eea7597ab22395
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-03-09 15:59:00 +0530

    Set LoginComponent as default route

--- Frontend/bitezza/src/app/app.routes.ts
+++ Frontend/bitezza/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
 
 export const routes: Routes = [
 
   {
     path:'',
+    component: LogInComponent
+  },
+  {
+    path: 'home',
     component: HomeComponent
   }
 
